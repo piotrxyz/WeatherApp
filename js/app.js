f40a07a6798e8250c04b848232a3774ad14d43a6
piotrxyz <piotr.zalom@gmail.com>
2020-07-02 21:56:48 +0200

    add styles, class names edit, fetch fix

--- js/app.js
+++ js/app.js
@@ -1,28 +1,37 @@
 window.addEventListener('load', () => {
-    let long;
-    let lat;
-    let temperatureDescription = document.querySelector('.temperature-description');
-    let temperatureDegree = document.querySelector('.temperature-degree');
-    let locationTimezone = document.querySelector('.location-timezone');
+  let long;
+  let lat;
+  let weatherDescription = document.querySelector('.weather__description');
+  let weatherDegree = document.querySelector('.weather__degree');
+  let locationName = document.querySelector('.location__name');
 
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(position => {
-            long = position.coords.longitude;
-            lat = position.coords.latitude;
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(position => {
+      long = position.coords.longitude;
+      lat = position.coords.latitude;
+      const api = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=2ef5bd8ccaf308e15a25ffe2f7d1c326`;
 
-            const api = `https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=2ef5bd8ccaf308e15a25ffe2f7d1c326`;
-            
-            fetch(api)
-              .then(response => {
-                return response.json();
-              })
-                .then(data => {
-                  console.log(data);
-                  // const { temp, summary } = data.currently;
-                  // Set DOM Elements from the API
-                  // tempDegree.textContent = temp;
-                  // tempDescription.textContent = summary;
-                });
+      fetch(api)
+        .then(response => {
+          return response.json();
+        })
+        .then(data => {
+          console.log(data);
+
+          // converting Kelvin to Celcius
+          const temp = Math.round(parseFloat(data.main.temp) - 273.15);
+
+          // const fahrenheit = Math.round(((parseFloat(data.main.temp)-273.15)*1.8)+32);
+
+
+          const description = data.weather[0].description;
+
+          // Set DOM Elements from the API
+
+          weatherDegree.innerHTML = temp + '&deg;';
+          weatherDescription.textContent = description;
+          locationName.textContent = data.name;
         });
-    }
+    });
+  }
 });
