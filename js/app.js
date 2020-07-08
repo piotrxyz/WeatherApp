976c78c6f1a3eee68fa2a9ec50697a8d3c4030e5
piotrxyz <piotr.zalom@gmail.com>
2020-07-08 21:30:42 +0200

    icons add, rwd add, delete assets, README.md edit

--- js/app.js
+++ js/app.js
@@ -5,6 +5,7 @@ window.addEventListener('load', () => {
   let locationDescription = document.querySelector('.location__description');
   let locationDegree = document.querySelector('.location__degree');
   let locationName = document.querySelector('.location__name');
+  const setIcon = document.querySelector('.location__icon');
 
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(position => {
@@ -21,20 +22,15 @@ window.addEventListener('load', () => {
 
           // converting Kelvin to Celcius
           const temp = Math.round(parseFloat(data.main.temp) - 273.15);
-
-          // const fahrenheit = Math.round(((parseFloat(data.main.temp)-273.15)*1.8)+32);
-
-
           const description = data.weather[0].description;
-          const icon = data.weather[0].icon;
-          console.log(data.weather[0].icon);
+          const { weather, name } = data;
+          const icon = `https://openweathermap.org/img/wn/${weather[0]["icon"]}@2x.png`;
 
           // Set DOM Elements from the API
-
           locationDegree.innerHTML = temp + '&deg;C';
           locationDescription.textContent = description;
-          locationName.textContent = data.name;
-
+          locationName.textContent = name;
+          setIcon.src = icon;
         });
     });
   }
